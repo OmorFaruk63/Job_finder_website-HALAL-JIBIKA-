0360aca7d9a3f161bf912adbb387451d2fc03c8e
OmorFaruk63 <omor3710@gmail.com>
2024-01-04 09:47:14 +0600

    vercel routing problem solve  and contact page form change to react hook form

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,56 +1,58 @@
-import { useState } from "react";
-import "./Contact.css";
+import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
+import "./Contact.css";
 
 const Contact = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    setName("");
-    setEmail("");
-    setMessage("");
-    toast.success("Thank you for contact us");
-  }
+  const onSubmit = (data) => {
+    setValue("name", "");
+    setValue("email", "");
+    setValue("message", "");
+    toast.success("Thank you for contacting us");
+  };
 
   return (
     <div className="contact-page">
       <div className="contact-form-container">
         <h2>Contact Us</h2>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="form-group">
             <label htmlFor="name">Name:</label>
             <input
-              value={name}
+              {...register("name", { required: "Name is required" })}
               type="text"
               placeholder="Your Name"
-              onChange={(e) => setName(e.target.value)}
-              required
             />
+            {errors.name && <p>{errors.name.message}</p>}
           </div>
 
           <div className="form-group">
             <label htmlFor="email">Email:</label>
             <input
-              value={email}
+              {...register("email", {
+                required: "Email is required",
+                pattern: /^\S+@\S+$/i,
+              })}
               type="email"
-              onChange={(e) => setEmail(e.target.value)}
               placeholder="Your Email"
-              required
             />
+            {errors.email && <p>{errors.email.message}</p>}
           </div>
 
           <div className="form-group">
             <label htmlFor="message">Message:</label>
             <textarea
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
+              {...register("message", { required: "Message is required" })}
               placeholder="Your Message"
               rows="4"
-              required
             />
+            {errors.message && <p>{errors.message.message}</p>}
           </div>
 
           <button className="contact-btn" type="submit">
